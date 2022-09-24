e70be4ae6b7dd22ad298395016742ce32f7a8dcc
nicole mccabe <thisisnotnicole@gmail.com>
2022-09-24 13:11:31 -0400

    initial commit

---
+++ pages/travel.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next'
+
+const Travel: NextPage = () => {
+  return <div>
+      <h1 className="text-3xl font-bold underline">Travel</h1>
+    </div>
+}
+
+export default Travel;
