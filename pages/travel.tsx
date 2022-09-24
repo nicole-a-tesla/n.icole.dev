0b6b6a3edc6e3050ac8a79c893124437e1b87973
nicole mccabe <thisisnotnicole@gmail.com>
2022-09-24 13:48:47 -0400

    basic map

--- pages/travel.tsx
+++ pages/travel.tsx
@@ -1,8 +1,19 @@
 import type { NextPage } from 'next'
+import Map, { Marker } from 'react-map-gl';
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
 
 const Travel: NextPage = () => {
-  return <div>
-      <h1 className="text-3xl font-bold underline">Travel</h1>
+  return <div style={{width: "100vw", height: "100vh"}}>
+      <Map 
+        style={{width: "100%", height: "100%"}}
+        mapStyle="https://demotiles.maplibre.org/style.json"
+        mapLib={maplibregl}>
+
+        <Marker longitude={0} latitude={0} anchor="bottom" >
+        </Marker>
+
+      </Map>;
     </div>
 }
 
