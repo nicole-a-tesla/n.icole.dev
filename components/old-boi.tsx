b88aa3f647a7c287963640bf1f4d514afd980701
nicole mccabe <thisisnotnicole@gmail.com>
2025-03-11 13:57:58 +0000

    use tailwind classes where possible

--- components/old-boi.tsx
+++ components/old-boi.tsx
@@ -4,47 +4,38 @@ const OldBoi = () => {
     const headStyle = {
         transform: 'translate(180px, 35px)',
         width: '180px',
-        zIndex: '3'
     }
 
-    const bodyStyle = {
-        zIndex: '2'
-    }
     const leftArmStyle = {
         translate: '300px -145px',
         width: '115px',
-        zIndex: '1',
-        transformOrigin: '10px'
     }
     const rightArmStyle = {
         translate: '250px -180px',
         width: '185px',
-        zIndex: '4',
-        transformOrigin: '20px',
     }
     const laptopStyle = {
         width: '250px',
         transform: 'translate(290px, -240px)',
-        zIndex: '0'
     }
 
     return (
         <div style={{minWidth: '448px' }} className="-translate-x-[75px] md:-translate-x-[30px]">
-            <img style={headStyle} className="absolute" src="/OB-head.png" />
-            <img style={bodyStyle} className="relative" src="/OB1-body.png" />
+            <img style={headStyle} className="absolute z-3" src="/OB-head.png" />
+            <img className="relative z-2" src="/OB1-body.png" />
             <motion.img
                 style={leftArmStyle}
                 animate={{ rotate: -20 }}
                 transition={{ repeat: Infinity, repeatType: "reverse", bounce: 0, duration: 0.1 }}
-                className="absolute"
+                className="absolute z-1 origin-[10px]"
                 src="/OB-left-arm.png" />
             <motion.img
                 style={rightArmStyle}
                 animate={{ rotate: 20 }}
                 transition={{ repeat: Infinity, repeatType: "reverse", bounce: 0, duration: 0.1 }}
-                className="absolute"
+                className="absolute z-4 origin-[20px]"
                 src="/OB-right-arm.png" />
-            <img style={laptopStyle} className="absolute" src="/laptop.png" />
+            <img style={laptopStyle} className="absolute z-0" src="/laptop.png" />
         </div>
     )
 }
